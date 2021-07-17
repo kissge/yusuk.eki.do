69c3524425044e59fb142e7826ce8750ecd531da
Yusuke Kido <mail@yusuk.eki.do>
2021-07-17 12:05:44 +0900

    developer

--- script/main.ts
+++ script/main.ts
@@ -1,4 +1,20 @@
-const monitor = document.querySelector('.monitor');
+const argMinIndex = <T>(items: T[], key: (item: T) => number) => {
+  const keys = items.map(key);
+  const min = Math.min(...keys);
+  return keys.indexOf(min);
+};
+
+window.addEventListener('scroll', (event) => {
+  const scrollY = window.scrollY;
+  const closest = argMinIndex(
+    Array.from(document.querySelectorAll<HTMLElement>('section:not(:nth-of-type(1))')),
+    (section) => Math.abs(section.offsetTop - scrollY),
+  );
+
+  document.querySelectorAll<HTMLElement>('.monitor').forEach((m, i) => m.classList.toggle('hide', i !== closest));
+});
+
+const monitor = document.querySelector('.monitor.developer');
 const words = document.querySelector('html').outerHTML;
 let i = 0;
 setInterval(() => {
