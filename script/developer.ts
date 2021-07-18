f0ab74fef1ce34e7ce8bd68cfe83d13ad7b4c1ff
Yusuke Kido <mail@yusuk.eki.do>
2021-07-18 14:30:46 +0900

    微調整

--- script/developer.ts
+++ script/developer.ts
@@ -3,6 +3,6 @@ const words = document.querySelector('html').outerHTML;
 let i = 0;
 setInterval(() => {
   monitor.textContent = words.slice(0, i);
-  i = (i + 1) % words.length;
+  i = (i + 10) % words.length;
   monitor.scrollBy(0, 10000);
 }, 10);
