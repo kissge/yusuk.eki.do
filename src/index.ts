e05b0a88a3922e41007135f061cd00f7a68a27b6
Yusuke Kido <mail@yusuk.eki.do>
2024-01-20 18:15:15 +0900

    Perf. optimization

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
 import './card/card';
 import './works/works';
 import './gtag';
+
+document.addEventListener('DOMContentLoaded', () => {
+  setTimeout(() => {
+    document.querySelectorAll('[loading="lazy"]').forEach((el) => {
+      el.removeAttribute('loading');
+    });
+  }, 2000);
+});
